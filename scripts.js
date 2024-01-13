66fdc955f818c9281ecf269af339e5db3188a730
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-01-13 16:45:32 +0800

    script change

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const apiKey = process.env.NEWS_API_KEY;
-const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${a10d56a575c34c148fe233632eeb55d3}`;
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${a10d56a575c34c148fe233632eeb55d3}`
 
 async function fetchNews(){
     try{
-        const response = await fetch(url)
+        const response = await fetch(url);
         const data = await response.json();
         console.log(data);
         // add function call
